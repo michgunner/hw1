5f4b46afa1369195df764e289046b702de9d45c6
michgunner <kron.thunder@outlook.com>
2022-11-29 16:04:21 +0300

    Task 3 done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿// Homework 1
 
-// Task2. You recieve 2 numbers. Find max
+// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
 /*
 Console.Write("Please, enter first number: ");
@@ -13,7 +13,7 @@ if (firstNum > secoundNum)
 else Console.Write("Maximum number is secound and it equals - " + secoundNum);
 */
 
-//Task 4. You recieve 3 numbers. Find max
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 /*
 Console.Write("Please, enter first number: ");
@@ -35,3 +35,10 @@ if (firstNum > secoundNum)
    else Console.Write("Maximum number is third and it equals - " + thirdNum);
 }
 */
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Please, enter your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 ==0) Console.WriteLine("Your number is even");
+else Console.WriteLine("Your number is not even");
