af8bbd6fbe517870e735a2527497276b7e61b04b
michgunner <kron.thunder@outlook.com>
2022-11-29 15:47:48 +0300

    Second task done

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// Homework 1
 
 // Task2. You recieve 2 numbers. Find max
+
+/*
 Console.Write("Please, enter first number: ");
 int firstNum = Convert.ToInt32(Console.ReadLine());
 Console.Write("Please, enter secound number: ");
@@ -9,3 +11,27 @@ int secoundNum = Convert.ToInt32(Console.ReadLine());
 if (firstNum > secoundNum) 
    Console.Write("Maximum number is first and it equals - " + firstNum);
 else Console.Write("Maximum number is secound and it equals - " + secoundNum);
+*/
+
+//Task 4. You recieve 3 numbers. Find max
+
+/*
+Console.Write("Please, enter first number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please, enter secound number: ");
+int secoundNum = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please, enter third number: ");
+int thirdNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum > secoundNum)
+{
+   if (firstNum > thirdNum)
+      Console.Write("Maximum number is first and it equals - " + firstNum);
+   else Console.Write("Maximum number is third and it equals - " + thirdNum);
+} else 
+{
+   if (secoundNum > thirdNum)
+      Console.Write("Maximum number is second and it equals - " + secoundNum);
+   else Console.Write("Maximum number is third and it equals - " + thirdNum);
+}
+*/
